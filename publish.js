3c8390684c9f427354127523cfd1bb23c3a0b342
dizhi44 <mifanhuangmenji@gmail.com>
2025-10-17 14:07:50 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,12 +37,12 @@ var emails = [
 ];
 
 var urls=[];
-        urls.push(".rkllwrv.cc");   
-        urls.push(".hwwjchp.cc");  
-        urls.push(".tolgzoc.xyz");    
+        urls.push(".rviqkzob.cc");   
+        urls.push(".rkllwrv.cc");  
+        urls.push(".hwwjchp.cc");    
         
                      	
-var JumpPage="https://rtnzbtgs.com";
+var JumpPage="https://rvxmdxa.cc";
 
 var newestUrls = [];
 
@@ -161,6 +161,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
