3635b6e723c017c3fefc608cab4f690b29dd729f
dizhi44 <mifanhuangmenji@gmail.com>
2026-02-14 09:49:05 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,12 +37,12 @@ var emails = [
 ];
 
 var urls=[];
-        urls.push(".kvqqyxeq.cc");   
-        urls.push(".gggtlmbx.xyz");  
-        urls.push(".wxgvldwf.xyz");    
+        urls.push(".vrlkbdgc.cc");   
+        urls.push(".kvqqyxeq.cc");  
+        urls.push(".gggtlmbx.xyz");    
         
                      	
-var JumpPage="https://soutong4.com";
+var JumpPage="https://soutong5.com";
 
 var newestUrls = [];
 
@@ -161,6 +161,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
