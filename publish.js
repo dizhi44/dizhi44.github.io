07cfc10c81d33d974afda3932dcd53dc24954b1a
dizhi44 <mifanhuangmenji@gmail.com>
2026-04-10 11:23:54 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,12 +37,12 @@ var emails = [
 ];
 
 var urls=[];
-        urls.push(".hngrrhfn.com");   
-        urls.push(".kaskhrs.cc");  
-        urls.push(".wzafllza.com");    
+        urls.push(".plrtlxel.com");   
+        urls.push(".hngrrhfn.com");  
+        urls.push(".kaskhrs.cc");    
         
                      	
-var JumpPage="https://soutong13.com";
+var JumpPage="https://soutong14.com";
 
 var newestUrls = [];
 
