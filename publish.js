9fe5dbf63210ef2a899bd7d465f036297bdc4248
dizhi44 <52soutong@gmail.com>
2026-07-21 12:03:40 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,12 +37,12 @@ var emails = [
 ];
 
 var urls=[];
-        urls.push(".leiwzcrcm.cc");   
-        urls.push(".hmspsdwf.com");  
-        urls.push(".wrveeaelk.cc");    
+        urls.push(".qxvwrsiyz.cc");   
+        urls.push(".leiwzcrcm.cc");  
+        urls.push(".hmspsdwf.com");    
         
                      	
-var JumpPage="https://dnalpugxg.cc";
+var JumpPage="https://rpqlkhulf.cc";
 
 var newestUrls = [];
 
