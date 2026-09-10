f55aded689f17ddb1bbef4385b29da678e89b698
dizhi44 <52soutong@gmail.com>
2026-09-10 20:45:35 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,9 +37,9 @@ var emails = [
 ];
 
 var urls=[];
-        urls.push(".gyjxtvzpw.cc");   
-        urls.push(".vflbreqq.cc");  
-        urls.push(".pvmzhavb.cc");    
+        urls.push(".jvwdgldg.com");   
+        urls.push(".gyjxtvzpw.cc");  
+        urls.push(".vflbreqq.cc");    
         
                      	
 var JumpPage="https://soutong27.com";
